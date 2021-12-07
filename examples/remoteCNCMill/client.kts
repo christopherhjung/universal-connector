295211d7d27e1feac714d42cfca7822feef7d29d
Christopher Jung <christopher.h.jung@web.de>
2021-12-07 09:18:00 +0100

    save

--- examples/remoteCNCMill/client.kts
+++ examples/remoteCNCMill/client.kts
@@ -7,7 +7,7 @@ config{
     input = StdInOut()
     output = Client("192.168.178.59",5555)
     //output = Loopback()
-    fileLoader("/Users/chris/Fusion 360 CAM/nc/")
+    fileLoader("add")
     timeLogging()
     okFilter()
     commands {
